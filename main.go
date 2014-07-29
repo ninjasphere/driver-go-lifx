d5dd9f60f2b6ca921544ae49a675dc70fe12de6d
wandergeek <innernetz@gmail.com>
2014-07-29 16:27:21 +1000

    rename commands.go to driver.go

--- main.go
+++ main.go
@@ -6,27 +6,10 @@ import (
 
 )
 
-var drivername = "driver-lifx"
-var log = ninja.GetLogger(drivername)
-
 func main() {
-
 	os.Exit(realMain())
 }
 
 func realMain() int {
-	// Get the command line args. We shortcut "--version" and "-v" to
-	// just show the version.
-	args := os.Args[1:]
-	for _, arg := range args {
-		if arg == "-v" || arg == "--version" {
-			newArgs := make([]string, len(args)+1)
-			newArgs[0] = "version"
-			copy(newArgs[1:], args)
-			args = newArgs
-			break
-		}
-	}
-
 	return run()
 }
