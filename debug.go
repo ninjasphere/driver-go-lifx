d49e597dd98ad744db297b5526265f29fe083942
Mark Wolfe <mark@wolfe.id.au>
2014-10-31 13:09:01 +1100

    Leave logging configuration to the logging package in go-ninja.

--- debug.go
+++ debug.go
@@ -2,14 +2,9 @@
 
 package main
 
-import (
-	"github.com/bugsnag/bugsnag-go"
-	"github.com/juju/loggo"
-	"github.com/ninjasphere/go-ninja/logger"
-)
+import "github.com/bugsnag/bugsnag-go"
 
 func init() {
-	logger.GetLogger("").SetLogLevel(loggo.DEBUG)
 
 	bugsnag.Configure(bugsnag.Configuration{
 		APIKey:       "57be7f895461a18014b0b325daf4ea3e",
--- release.go
+++ release.go
@@ -2,15 +2,9 @@
 
 package main
 
-import (
-	"github.com/bugsnag/bugsnag-go"
-	"github.com/juju/loggo"
-	"github.com/ninjasphere/go-ninja/logger"
-)
+import "github.com/bugsnag/bugsnag-go"
 
 func init() {
-	logger.GetLogger("").SetLogLevel(loggo.INFO)
-
 	bugsnag.Configure(bugsnag.Configuration{
 		APIKey:       "57be7f895461a18014b0b325daf4ea3e",
 		ReleaseStage: "production",
