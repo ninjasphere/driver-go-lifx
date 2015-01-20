d512e1e882d3098cb47bae9e933fbd0549aa1b9d
Mark Wolfe <mark@wolfe.id.au>
2015-01-20 14:01:34 +1100

    Moved to the new bugsnag reporter.

--- release.go
+++ release.go
@@ -2,11 +2,11 @@
 
 package main
 
-import "github.com/bugsnag/bugsnag-go"
+import "github.com/ninjasphere/go-ninja/bugs"
+
+// BugsKey key used for reporting bugs
+const BugsKey = "57be7f895461a18014b0b325daf4ea3e"
 
 func init() {
-	bugsnag.Configure(bugsnag.Configuration{
-		APIKey:       "57be7f895461a18014b0b325daf4ea3e",
-		ReleaseStage: "production",
-	})
+	bugs.Configure("release", BugSnagKey)
 }
