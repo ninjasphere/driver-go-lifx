22e7d3d6c741ea68c712ee17aceffbc34e8a4a83
Mark Wolfe <mark@wolfe.id.au>
2015-01-20 14:07:59 +1100

    Correct typo when accessing constant.

--- release.go
+++ release.go
@@ -8,5 +8,5 @@ import "github.com/ninjasphere/go-ninja/bugs"
 const BugsKey = "57be7f895461a18014b0b325daf4ea3e"
 
 func init() {
-	bugs.Configure("release", BugSnagKey)
+	bugs.Configure("release", BugsKey)
 }
